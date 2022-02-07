7647d1815ec43ddf477807b9968b40640c98076e
Shobhit Gautam <98753165+ShobhitGautam@users.noreply.github.com>
2022-02-07 11:51:31 +0530

    Day 4 Assignment Submitted

---
+++ Day 4.py.py
@@ -0,0 +1,10 @@
+# Assignment 4
+
+def factorial(n):
+    fact = 1
+    while 1 < n:
+        fact = fact * n
+        n -= 1
+    print("Factorial is :", fact)
+    
+factorial(int(input("Enter a number: ")))
