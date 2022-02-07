ff9607c207034377f81d0c6d00173102fe46cc9e
Shobhit Gautam <98753165+ShobhitGautam@users.noreply.github.com>
2022-02-07 11:49:41 +0530

    Day 1 Assgnment Submitted

---
+++ Day 1.py.py
@@ -0,0 +1,10 @@
+# Assignment 1
+# Take multiple numbers as input and print the sum.
+
+x = [int(i) for i in input('Enter integer values: ').split()]
+
+s = 0
+for j in x:
+  s += j # sum = sum + j
+
+print(s)
